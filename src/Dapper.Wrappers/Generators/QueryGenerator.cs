91515beedf22ded6863be841a6a6e0d673f74d06
Ben Skeen <benjamin.skeen@healthcatalyst.com>
2021-08-04 22:49:42 -0600

    I continued to work on the QueryGenerator tests.

--- src/Dapper.Wrappers/Generators/QueryGenerator.cs
+++ src/Dapper.Wrappers/Generators/QueryGenerator.cs
@@ -27,7 +27,7 @@ namespace Dapper.Wrappers.Generators
         /// <summary>
         /// Formats the given operations using the given format function.
         /// </summary>
-        /// <typeparam name="OMT">The type of QueryOperationMetadata to use while formatting the operation.</typeparam>
+        /// <typeparam name="TOpMetadata">The type of QueryOperationMetadata to use while formatting the operation.</typeparam>
         /// <param name="context">The context to be updated.</param>
         /// <param name="operations">The operations to format.</param>
         /// <param name="operationMetadata">The metadata about allowed operations.</param>
@@ -36,11 +36,11 @@ namespace Dapper.Wrappers.Generators
         /// <param name="checkOrdering">Whether or not to check for an ordering parameter.</param>
         /// <param name="useUniqueVariables">Whether or not to add unique variables to the context.</param>
         /// <returns></returns>
-        protected virtual List<string> FormatOperations<OMT>(IQueryContext context, IEnumerable<QueryOperation> operations,
-            IDictionary<string, OMT> operationMetadata,
-            Func<string, IEnumerable<string>, OrderDirections?, string> formatOperation, Action<OMT> operationAction,
+        protected virtual List<string> FormatOperations<TOpMetadata>(IQueryContext context, IEnumerable<QueryOperation> operations,
+            IDictionary<string, TOpMetadata> operationMetadata,
+            Func<string, IEnumerable<string>, OrderDirections?, string> formatOperation, Action<TOpMetadata> operationAction,
             bool checkOrdering = false, bool useUniqueVariables = true)
-            where OMT : QueryOperationMetadata
+            where TOpMetadata : QueryOperationMetadata
         {
             List<string> formattedOperations = new List<string>();
 
@@ -80,7 +80,7 @@ namespace Dapper.Wrappers.Generators
                     }
                     else
                     {
-                        var variableName = context.AddVariable(parameter.Name, parameterValue, parameter.DbType);
+                        var variableName = context.AddVariable(parameter.Name, parameterValue, parameter.DbType, useUniqueVariables);
                         parameterNames.Add(variableName);
                     }
                 }
--- tests/Dapper.Wrappers.Tests/Generators/BaseQueryGeneratorTests.cs
+++ tests/Dapper.Wrappers.Tests/Generators/BaseQueryGeneratorTests.cs
@@ -2,16 +2,348 @@
 // Licensed to be used under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dapper.Wrappers.DependencyInjection;
 using Dapper.Wrappers.Formatters;
 using Dapper.Wrappers.Generators;
+using FluentAssertions;
+using Moq;
+using Xunit;
 
 namespace Dapper.Wrappers.Tests.Generators
 {
     public class BaseQueryGeneratorTests
     {
-        private static QueryGenerator GetDefaultTestInstance(IQueryFormatter queryFormatter)
+        private static TestQueryGenerator GetDefaultTestInstance(SupportedDatabases dbType)
         {
-            return new TestQueryGenerator(queryFormatter);
+            IQueryFormatter formatter = dbType == SupportedDatabases.SqlServer
+                ? (IQueryFormatter)(new SqlServerQueryFormatter())
+                : new PostgresQueryFormatter();
+            return new TestQueryGenerator(formatter);
+        }
+
+        private static Mock<IQueryContext> GetMockQueryContext()
+        {
+            Random generator = new Random();
+            var result = new Mock<IQueryContext>();
+            result.Setup(context => context.AddVariable(It.IsAny<string>(), It.IsAny<object>(),
+                It.Is<DbType?>((obj, type) => type == typeof(DbType?)), It.IsAny<bool>())).Returns(
+                (string name, object value, DbType? type, bool isUnique) => $"{name}{generator.Next(1000)}");
+
+            return result;
+        }
+
+        [Theory]
+        [InlineData(true, SupportedDatabases.SqlServer)]
+        [InlineData(false, SupportedDatabases.SqlServer)]
+        [InlineData(true, SupportedDatabases.PostgreSQL)]
+        [InlineData(false, SupportedDatabases.PostgreSQL)]
+        public void FormatOperations_WithNullOrEmptyInputs_ShouldReturnAnEmptyList(bool isNull, SupportedDatabases dbType)
+        {
+            // Arrange
+            var generator = GetDefaultTestInstance(dbType);
+            var mockContext = GetMockQueryContext();
+            IEnumerable<QueryOperation> operations = isNull ? null : new List<QueryOperation>();
+
+            // Act
+            var results =
+                generator.TestFormatOperations<QueryOperationMetadata>(mockContext.Object, operations, null, null,
+                    null);
+
+            // Assert
+            results.Should().NotBeNull();
+            results.Should().BeEmpty();
+            mockContext.Verify(
+                context => context.AddVariable(It.IsAny<string>(), It.IsAny<object>(), It.IsAny<DbType>(),
+                    It.IsAny<bool>()), Times.Never);
+        }
+
+        [Theory]
+        [InlineData(SupportedDatabases.SqlServer)]
+        [InlineData(SupportedDatabases.PostgreSQL)]
+        public void FormatOperations_WithBogusOperators_ShouldReturnAnEmptyList(SupportedDatabases dbType)
+        {
+            // Arrange
+            var generator = GetDefaultTestInstance(dbType);
+            var mockContext = GetMockQueryContext();
+
+            var operations = new[]
+            {
+                new QueryOperation
+                {
+                    Name = "Bogus1",
+                    Parameters = new Dictionary<string, object>
+                    {
+                        { "BogusValue1", null },
+                        { "BogusValue2", true }
+                    }
+                },
+                new QueryOperation
+                {
+                    Name = "Bogus2",
+                    Parameters = new Dictionary<string, object>
+                    {
+                        { "EvenMoreBogusValue1", 1 },
+                        { "EvenMoreBogusValue2", Guid.NewGuid() }
+                    }
+                }
+            };
+
+            var operationMetadata = new Dictionary<string, QueryOperationMetadata>
+            {
+                {
+                    "Real1",
+                    new QueryOperationMetadata
+                    {
+                        Name = "Real1",
+                        BaseQueryString = "this is not a query",
+                        Parameters = new QueryParameterMetadata[] { }
+                    }
+                },
+                {
+                    "Real2",
+                    new QueryOperationMetadata
+                    {
+                        Name = "Real2",
+                        BaseQueryString = "this is not a query either",
+                        Parameters = new QueryParameterMetadata[] { }
+                    }
+                }
+            };
+
+            // Act
+            var results = generator.TestFormatOperations(mockContext.Object, operations, operationMetadata, null, null);
+
+            // Assert
+            results.Should().NotBeNull();
+            results.Should().BeEmpty();
+
+            mockContext.Verify(
+                context => context.AddVariable(It.IsAny<string>(), It.IsAny<object>(), It.IsAny<DbType>(),
+                    It.IsAny<bool>()), Times.Never);
+        }
+
+        [Theory]
+        [InlineData(SupportedDatabases.SqlServer)]
+        [InlineData(SupportedDatabases.PostgreSQL)]
+        public void FormatOperations_WithOperationInputs_ShouldCallTheOperationActionForEachValidOperation(
+            SupportedDatabases dbType)
+        {
+            // Arrange
+            var generator = GetDefaultTestInstance(dbType);
+            var mockContext = GetMockQueryContext();
+
+            var operations = new[]
+            {
+                new QueryOperation
+                {
+                    Name = "Bogus1",
+                    Parameters = new Dictionary<string, object>
+                    {
+                        { "BogusValue1", null },
+                        { "BogusValue2", true }
+                    }
+                },
+                new QueryOperation
+                {
+                    Name = "Valid1",
+                    Parameters = new Dictionary<string, object>
+                    {
+                        { "Valid1Value1", 1 },
+                        { "Valid1Value2", Guid.NewGuid() }
+                    }
+                },
+                new QueryOperation
+                {
+                    Name = "Valid2",
+                    Parameters = new Dictionary<string, object>
+                    {
+                        { "Valid2Value1", "Test String"},
+                        { "Valid2Value2", 233.4M }
+                    }
+                }
+            };
+
+            var operationMetadata = new Dictionary<string, QueryOperationMetadata>
+            {
+                {
+                    "Valid1",
+                    new QueryOperationMetadata
+                    {
+                        Name = "Valid1",
+                        BaseQueryString = "Value1: {0}, Value2: {1}",
+                        Parameters = new[]
+                        {
+                            new QueryParameterMetadata
+                            {
+                                Name = "Valid1Value1",
+                                DbType = DbType.Int32,
+                                HasDefault = false
+                            },
+                            new QueryParameterMetadata
+                            {
+                                Name = "Valid1Value2",
+                                DbType = DbType.Guid,
+                                HasDefault = false
+                            }
+                        }
+                    }
+                },
+                {
+                    "Valid2",
+                    new QueryOperationMetadata
+                    {
+                        Name = "Valid2",
+                        BaseQueryString = "Value1: {0}, Value2: {1}",
+                        Parameters = new[]
+                        {
+                            new QueryParameterMetadata
+                            {
+                                Name = "Valid2Value1",
+                                DbType = DbType.String,
+                                HasDefault = false
+                            },
+                            new QueryParameterMetadata
+                            {
+                                Name = "Valid2Value2",
+                                DbType = DbType.Decimal,
+                                HasDefault = false
+                            }
+                        }
+                    }
+                }
+            };
+
+            List<QueryOperationMetadata> collectedMetadata = new List<QueryOperationMetadata>();
+
+            // Act
+            var results = generator.TestFormatOperations(mockContext.Object, operations, operationMetadata,
+                generator.TestGetNonOrderingFormatOperation(generator.TestFormatter.FormatFilterOperation), TestAction);
+
+            // Assert
+            foreach (var result in results)
+            {
+                result.Should().MatchRegex("Value1: @Valid[12]Value1[0-9]*, Value2: @Valid[12]Value2[0-9]*");
+            }
+
+            collectedMetadata.Should().HaveCount(2);
+            var sortedMetadata = collectedMetadata.OrderBy(m => m.Name);
+            var sortedInputMetadata = operationMetadata.Values.OrderBy(m => m.Name);
+
+            sortedMetadata.Should().Equal(sortedInputMetadata,
+                (m1, m2) => m1.Name == m2.Name && m1.BaseQueryString == m2.BaseQueryString);
+
+            mockContext.Verify(
+                context => context.AddVariable(It.IsAny<string>(), It.IsAny<object>(), It.IsAny<DbType>(),
+                    It.IsAny<bool>()), Times.Exactly(4));
+
+            void TestAction(QueryOperationMetadata metadata)
+            {
+                collectedMetadata.Add(metadata);
+            }
+        }
+
+        [Theory]
+        [InlineData(SupportedDatabases.SqlServer)]
+        [InlineData(SupportedDatabases.PostgreSQL)]
+        public void FormatOperations_WithMissingParametersWithDefault_ShouldStillReturnResults(
+            SupportedDatabases dbType)
+        {
+
+            // Arrange
+            var generator = GetDefaultTestInstance(dbType);
+            var mockContext = GetMockQueryContext();
+
+            var operations = new[]
+            {
+                new QueryOperation
+                {
+                    Name = "Bogus1",
+                    Parameters = new Dictionary<string, object>
+                    {
+                        { "BogusValue1", null },
+                        { "BogusValue2", true }
+                    }
+                },
+                new QueryOperation
+                {
+                    Name = "Valid1",
+                    Parameters = new Dictionary<string, object>()
+                },
+                new QueryOperation
+                {
+                    Name = "Valid2",
+                    Parameters = new Dictionary<string, object>()
+                }
+            };
+
+            var operationMetadata = new Dictionary<string, QueryOperationMetadata>
+            {
+                {
+                    "Valid1",
+                    new QueryOperationMetadata
+                    {
+                        Name = "Valid1",
+                        BaseQueryString = "Value1: {0}, Value2: {1}",
+                        Parameters = new[]
+                        {
+                            new QueryParameterMetadata
+                            {
+                                Name = "Valid1Value1",
+                                DbType = DbType.Int32,
+                                HasDefault = true,
+                                DefaultValue = 1
+                            },
+                            new QueryParameterMetadata
+                            {
+                                Name = "Valid1Value2",
+                                DbType = DbType.Guid,
+                                HasDefault = true,
+                                DefaultValue = Guid.NewGuid()
+                            }
+                        }
+                    }
+                },
+                {
+                    "Valid2",
+                    new QueryOperationMetadata
+                    {
+                        Name = "Valid2",
+                        BaseQueryString = "Value1: {0}, Value2: {1}",
+                        Parameters = new[]
+                        {
+                            new QueryParameterMetadata
+                            {
+                                Name = "Valid2Value1",
+                                DbType = DbType.String,
+                                HasDefault = true,
+                                DefaultValue = "Test String"
+                            },
+                            new QueryParameterMetadata
+                            {
+                                Name = "Valid2Value2",
+                                DbType = DbType.Decimal,
+                                HasDefault = true,
+                                DefaultValue = 233.4M
+                            }
+                        }
+                    }
+                }
+            };
+
+            // Act
+            var results = generator.TestFormatOperations(mockContext.Object, operations, operationMetadata,
+                generator.TestGetNonOrderingFormatOperation(generator.TestFormatter.FormatFilterOperation),
+                generator.TestNoopOperationAction);
+
+            // Assert
+            foreach (var result in results)
+            {
+                result.Should().MatchRegex("Value1: @Valid[12]Value1[0-9]*, Value2: @Valid[12]Value2[0-9]*");
+            }
         }
     }
 
@@ -19,6 +351,23 @@ namespace Dapper.Wrappers.Tests.Generators
     {
         public TestQueryGenerator(IQueryFormatter queryFormatter) : base(queryFormatter)
         {
+        }
+
+        public IQueryFormatter TestFormatter => QueryFormatter;
+
+        public List<string> TestFormatOperations<TOpMetadata>(IQueryContext context,
+            IEnumerable<QueryOperation> operations, IDictionary<string, TOpMetadata> operationMetadata,
+            Func<string, IEnumerable<string>, OrderDirections?, string> formatOperation,
+            Action<TOpMetadata> operationAction, bool checkOrdering = false, bool useUniqueVariables = true)
+            where TOpMetadata : QueryOperationMetadata => FormatOperations(context, operations, operationMetadata,
+            formatOperation, operationAction, checkOrdering, useUniqueVariables);
+
+        public void TestNoopOperationAction(QueryOperationMetadata metadata){}
+
+        public Func<string, IEnumerable<string>, OrderDirections?, string> TestGetNonOrderingFormatOperation(
+            Func<string, IEnumerable<string>, string> operation)
+        {
+            return (operationString, variableNames, orderDirection) => operation(operationString, variableNames);
         }
     }
 }
