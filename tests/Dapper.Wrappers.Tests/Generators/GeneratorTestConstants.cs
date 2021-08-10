ada3e279752ca4ef1d7260fc37bb9c9ba1a63131
Ben Skeen <benjamin.skeen@healthcatalyst.com>
2021-08-09 19:53:53 -0600

    I added more metadata to the generator test constants.

--- tests/Dapper.Wrappers.Tests/Generators/GeneratorTestConstants.cs
+++ tests/Dapper.Wrappers.Tests/Generators/GeneratorTestConstants.cs
@@ -15,8 +15,17 @@ namespace Dapper.Wrappers.Tests.Generators
                 new Dictionary<string, QueryOperationMetadata>
                 {
                     {
-                        "NameLike",
-                        _metadataGenerator.GetDefaultOperation<string>("NameLike", "[Name] LIKE {0}")
+                        "BookIDEquals",
+                        _metadataGenerator.GetDefaultOperation<Guid>("BookIDEquals", "[BookID] = {0}")
+                    },
+                    {
+                        "BookIDNotEquals",
+                        _metadataGenerator.GetDefaultOperation<Guid>("BookIDNotEquals", "[BookID] <> {0}")
+                    },
+                    {
+                        "BookIDIn",
+                        _metadataGenerator.GetOperation("BookIDIn", "BookID IN {0}",
+                            new[] {_metadataGenerator.GetParameter("BookID", null)})
                     },
                     {
                         "NameEquals",
@@ -27,8 +36,51 @@ namespace Dapper.Wrappers.Tests.Generators
                         _metadataGenerator.GetDefaultOperation<string>("NameNotEquals", "[Name] <> {0}")
                     },
                     {
+                        "NameIn",
+                        _metadataGenerator.GetOperation("NameIn", "[Name] IN {0}",
+                            new[] {_metadataGenerator.GetParameter("Name", null)})
+                    },
+                    {
+                        "NameLike",
+                        _metadataGenerator.GetDefaultOperation<string>("NameLike", "[Name] LIKE {0}")
+                    },
+                    {
+                        "AuthorIDEquals",
+                        _metadataGenerator.GetDefaultOperation<Guid>("AuthorIDEquals", "[AuthorID] = {0}")
+                    },
+                    {
+                        "AuthorIDNotEquals",
+                        _metadataGenerator.GetDefaultOperation<Guid>("AuthorIDNotEquals", "[AuthorID] <> {0}")
+                    },
+                    {
+                        "AuthorIDIn",
+                        _metadataGenerator.GetOperation("AuthorIDIn", "[AuthorID] IN {0}",
+                            new[] {_metadataGenerator.GetParameter("AuthorID", null)})
+                    },
+                    {
                         "PageCountEquals",
                         _metadataGenerator.GetDefaultOperation<int>("PageCountEquals", "[PageCount] = {0}")
+                    },
+                    {
+                        "PageCountNotEquals",
+                        _metadataGenerator.GetDefaultOperation<int>("PageCountNotEquals", "[PageCount] <> {0}")
+                    },
+                    {
+                        "PageCountGreater",
+                        _metadataGenerator.GetDefaultOperation<int>("PageCountGreater", "[PageCount] > {0}")
+                    },
+                    {
+                        "PageCountLess",
+                        _metadataGenerator.GetDefaultOperation<int>("PageCountLess", "[PageCount] < {0}")
+                    },
+                    {
+                        "PageCountBetween",
+                        _metadataGenerator.GetOperation("PageCountBetween", "[PageCount] BETWEEN {0} AND {1}",
+                            new[]
+                            {
+                                _metadataGenerator.GetParameter<int>("Start"),
+                                _metadataGenerator.GetParameter<int>("End")
+                            })
                     }
                 };
         }
