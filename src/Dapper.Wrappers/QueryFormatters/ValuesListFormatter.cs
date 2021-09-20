30c82eaefb4a1fe9835a962144423cde49173af2
Ben Skeen <benjamin.skeen@healthcatalyst.com>
2021-09-19 22:57:57 -0600

    I continued to work on the ValuesListFormatter.

--- src/Dapper.Wrappers/QueryFormatters/ValuesListFormatter.cs
+++ src/Dapper.Wrappers/QueryFormatters/ValuesListFormatter.cs
@@ -6,7 +6,7 @@ using Dapper.Wrappers.OperationFormatters;
 
 namespace Dapper.Wrappers.QueryFormatters
 {
-    public class ValuesListFormatter : QueryFormatter<object>, IValuesListFormatter
+    public class ValuesListFormatter : QueryFormatter<List<MergeOperationMetadata>>, IValuesListFormatter
     {
         private readonly IQueryOperationFormatter _queryOperationFormatter;
 
@@ -28,12 +28,12 @@ namespace Dapper.Wrappers.QueryFormatters
             var requiredOperations = valuesListMetadata.Where(metadata => metadata.Value.IsRequired)
                 .ToDictionary(metadata => metadata.Key, metadata => metadata.Value);
 
-            List<string> formattedColumnNames = new List<string>();
+            var orderedMetadata = new List<MergeOperationMetadata>();
 
             var formattedOperations = FormatOperations(context, valuesListOperations, valuesListMetadata,
-                GetNonOrderingFormatOperation(_queryOperationFormatter.FormatInsertOperation), InsertOperationAction);
+                GetNonOrderingFormatOperation(_queryOperationFormatter.FormatInsertOperation), InsertOperationAction, orderedMetadata);
 
-            AddRequiredInsertOperations(context, requiredOperations, formattedColumnNames, formattedOperations);
+            AddRequiredInsertOperations(context, requiredOperations, formattedOperations);
 
             if (formattedOperations.Count == 0)
             {
@@ -42,6 +42,55 @@ namespace Dapper.Wrappers.QueryFormatters
 
             var columnList = QueryFormatter.FormatInsertColumns(formattedColumnNames);
             var operationList = QueryFormatter.FormatInsertOperations(formattedOperations);
+        }
+
+        private void InsertOperationAction(MergeOperationMetadata metadata, int index, List<MergeOperationMetadata> state)
+        {
+            if (requiredOperations.ContainsKey(metadata.ReferencedColumn))
+            {
+                requiredOperations.Remove(metadata.ReferencedColumn);
+            }
+
+            if (currentColumns.Contains(metadata.ReferencedColumn))
+            {
+                throw new ArgumentException("Cannot have multiple inserts into the same column.");
+            }
+
+            currentColumns.Add(metadata.ReferencedColumn);
+            formattedColumnNames.Add(QueryFormatter.FormatInsertColumn(metadata.ReferencedColumn));
+        }
+
+        /// <summary>
+        /// Once the requested insert operations have been processed, adds any additional operations that
+        /// are required for the insert.
+        /// </summary>
+        /// <param name="context">The query context to update.</param>
+        /// <param name="requiredOperations">The dictionary of required insert operations, if any.</param>
+        /// <param name="formattedColumnNames">The list of columns into which values will be inserted.</param>
+        /// <param name="formattedOperations">The list of insert operations.</param>
+        private void AddRequiredInsertOperations(IQueryContext context,
+            IDictionary<string, MergeOperationMetadata> requiredOperations, List<string> formattedColumnNames,
+            List<string> formattedOperations)
+        {
+            foreach (var requiredOperation in requiredOperations)
+            {
+                List<string> parameterNames = new List<string>();
+
+                foreach (var parameter in requiredOperation.Value.Parameters)
+                {
+                    if (!parameter.HasDefault)
+                    {
+                        throw new ArgumentException($"Value must be supplied for '{requiredOperation.Key}'.");
+                    }
+
+                    var variableName = context.AddVariable(parameter.Name, parameter.DefaultValue, parameter.DbType);
+
+                    parameterNames.Add(variableName);
+                }
+
+                formattedColumnNames.Add(QueryFormatter.FormatInsertColumn(requiredOperation.Key));
+                formattedOperations.Add(QueryFormatter.FormatInsertOperation(requiredOperation.Value.BaseQueryString, parameterNames));
+            }
         }
     }
 }
